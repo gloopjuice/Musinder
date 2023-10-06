5dac9160c81afa681c6cd59d6561ea1101f20e01
dpGGagainis <39499184+gloopjuice@users.noreply.github.com>
2023-10-06 07:12:48 +0000

    qweqweqw

--- src/views/page1.vue
+++ src/views/page1.vue
@@ -1,14 +1,103 @@
 <template>
-<Navbar/>
-viens
-</template>
-<script>
-import Navbar from '../components/Navbar.vue';
-
-export default{
-   components:{
-       Navbar
-   }
-}
-</script>
-<style scoped></style>
+    <Navbar />
+    <div class="container">
+      <div class="scrollable-box">
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>        
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>        
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+        salamalekum<br>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from '../components/Navbar.vue';
+  
+  export default {
+    components: {
+      Navbar
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; 
+  }
+  
+  .scrollable-box {
+    width: 90%; 
+    max-height: 80vh; 
+    overflow-y: scroll;
+    background-color: white;
+    border-radius: 1rem;
+    border: 2px solid #E6E6E6;
+    background: blue;
+  }
+  </style>
