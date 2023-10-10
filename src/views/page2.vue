0bd572896779e358e04ec9f8847c61145793fa77
dpGGagainis <39499184+gloopjuice@users.noreply.github.com>
2023-10-10 19:12:36 +0000

    asdasd

---
+++ src/views/page2.vue
@@ -0,0 +1,16 @@
+<template>
+<Navbar/>
+<ViewingArea/>
+</template>
+<script>
+import Navbar from '../components/Navbar.vue';
+import ViewingArea from '../components/ViewingArea.vue';
+export default {
+components: {
+  Navbar, ViewingArea
+}
+}
+</script>
+<style scoped>
+
+</style>
