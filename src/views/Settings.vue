2f553f138a25e3bb06a64fd80ba5f2fbf12a1738
dpGGagainis <39499184+gloopjuice@users.noreply.github.com>
2023-10-17 19:49:36 +0300

    Add files via upload

---
+++ src/views/Settings.vue
@@ -0,0 +1,14 @@
+<template>
+<Navbar/>
+<ViewingArea/>
+</template>
+<script>
+import Navbar from '../components/Navbar.vue';
+import ViewingArea from '../components/ViewingArea.vue';
+export default {
+components: {
+  Navbar, ViewingArea
+}
+}
+</script>
+<style scoped></style>
